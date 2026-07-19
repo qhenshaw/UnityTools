7979f37a1ff6f45369745df20bd90dce00626260
Quin Henshaw <qhenshaw@gmail.com>
2026-07-19 14:28:52 -0700

    added optional toggle for component hiding

--- SceneManagement/HideChildren.cs
+++ SceneManagement/HideChildren.cs
@@ -7,6 +7,7 @@ namespace SceneManagement
     public class HideChildren : MonoBehaviour
     {
         [field: SerializeField] public bool IsHidden { get; set; } = true;
+        [field: SerializeField] public bool HideComponents { get; set; } = false;
         [field: SerializeField] public bool ShowInPrefabMode { get; set; } = true;
 
         private static HideFlags _hiddenFlags = HideFlags.HideInHierarchy | HideFlags.HideInInspector;
@@ -14,44 +15,45 @@ namespace SceneManagement
 
         private void OnValidate()
         {
-            SetChildrenHideFlags(gameObject, IsHidden);
+            SetChildrenHideFlags(gameObject, IsHidden, HideComponents);
         }
 
         private void Awake()
         {
-            SetChildrenHideFlags(gameObject, IsHidden);
+            SetChildrenHideFlags(gameObject, IsHidden, HideComponents);
         }
 
         public void SetHidden(bool hidden)
         {
             IsHidden = hidden;
-            SetChildrenHideFlags(gameObject, hidden);
+            SetChildrenHideFlags(gameObject, hidden, HideComponents);
         }
 
         private void OnDestroy()
         {
             if(!Application.isPlaying)
             {
-                SetChildrenHideFlags(gameObject, false);
+                SetChildrenHideFlags(gameObject, false, false);
             }
         }
 
-        public static void SetChildrenHideFlags(GameObject gameObject, bool hidden)
+        public static void SetChildrenHideFlags(GameObject gameObject, bool hidden, bool hideComponents)
         {
-            HideFlags flags = hidden ? _hiddenFlags : _visibleFlags;
+            HideFlags transformFlags = hidden ? _hiddenFlags : _visibleFlags;
             Transform[] tranforms = gameObject.GetComponentsInChildren<Transform>();
             for (int i = 0; i < tranforms.Length; i++)
             {
                 if (tranforms[i].gameObject == gameObject) continue;
-                tranforms[i].gameObject.hideFlags = flags;
+                tranforms[i].gameObject.hideFlags = transformFlags;
             }
 
+            HideFlags componentFlags = (hidden && hideComponents) ? _hiddenFlags : _visibleFlags;
             Component[] components = gameObject.GetComponents<Component>();
             for (int i = 0; i < components.Length; i++)
             {
                 if (components[i] is Transform) continue;
                 if (components[i] is HideChildren) continue;
-                components[i].hideFlags = flags;
+                components[i].hideFlags = componentFlags;
             }
         }
     }
